e072dc27b2750f94cb627b0a07c9f24477669dd9
Cameron O'Rourke <cameron.orourke@mac.com>
2017-11-08 21:06:42 -0800

    Added error handling

--- src/Metric2.js
+++ src/Metric2.js
@@ -10,26 +10,28 @@ class Metric2 extends React.Component {
     super(props)
     this.state = {
       isLoading: true,
+      error: null,
       measureId: C.metric(this.props.metric)
     }
   }
 
   render() {
     const composeLabel = () => {
-      return this.state.isLoading ? 'Loading...' :
-        this.props.label ? this.props.label :
-          this.props.metric
+      return this.state.error ? this.state.error.status :
+        this.state.isLoading ? 'Loading...' :
+          this.props.label ? this.props.label :
+            this.props.metric
     };
 
     return (
-      <div className="metric">
+      <div className={"metric" + (this.state.error ? " metric--state-error" : "") }>
         <div className="metric-value">
           <Kpi
             projectId={this.props.project}
             measure={this.state.measureId}
             format="#.##a"
             onLoadingChanged={ (result) => {this.setState(result)} }
-            onError={ (err) => console.error(err) } />
+            onError={ (err) => { this.setState({error: err})} } />
         </div>
         <div className="metric-label">{ composeLabel() }</div>
       </div>
